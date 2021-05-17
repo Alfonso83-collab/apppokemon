e1d17d3dea0bc2e5016af627c3c5e2a7a5329d59
Alfonso83-collab <78817752+Alfonso83-collab@users.noreply.github.com>
2021-05-16 22:50:01 -0500

    apppok

---
+++ Pokedex/app/src/main/java/com/moviles/pokedex/PokeApiService.kt
@@ -0,0 +1,15 @@
+package com.moviles.pokedex
+
+import com.moviles.pokedex.PokeApiResponse
+import com.moviles.pokedex.Pokemon
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface PokeApiService {
+    @GET("pokemon/{id}")
+    fun getPokemonInfo(@Path("id") id: Int): Call<Pokemon>
+    @GET("pokemon")
+    fun getPokemonList(@Query("limit") limit: Int, @Query("offset") offset: Int): Call<PokeApiResponse>
+}
